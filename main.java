df1a417daa057f8eadd3d46675f577fb73cc14fe
willy <madreskimani001@gmail.com>
2025-09-23 11:02:28 +0300

    update 01

--- main.java
+++ main.java
@@ -1,13 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        int a ;
-        int b ;
-        System.in
-        int result = addNumbers(a, b);
-        System.out.println("Sum = " + result);
-    }
+public class Main{
+public static void main(String[] args){
 
-    static int addNumbers(int x, int y) {
-        return x + y;
-    }
+System.out.println("Hello Java");
+System.out.println(14*2);
+
+// this is a single line comment
+/* this is a multiline comment*/
+ 
+	//String
+String name = "ULiTheDev";
+System.out.println("Name is " + name);
+System.out.println("The name has " + name.length() +" characters");
+System.out.println("Name in uppercase is " + name.toUpperCase());
+System.out.println("Name in lowercase is " + name.toLowerCase());
+
+int age = 30;
+System.out.println("Age is " + age);
+
+float currentAmount;
+currentAmount = 150;
+System.out.println("Current amount in tha pocket is " + currentAmount);
+
+int a = 10,b = 12,c = a + b;
+System.out.println("The sum of " + a + " and " + b + " is " + c);
+
+}
 }
